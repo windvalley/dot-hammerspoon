23c10f4af24daffc31f95c49352ec23a90e34f19
windvalley <levinwang6@gmail.com>
2022-12-18 03:17:05 +0800

    Initial commit
    Add features:
    
    1. apps switch
    2. windows manage
    3. hotkeys show
    4. auto reload

---
+++ auto_reload.lua
@@ -0,0 +1,26 @@
+local _M = {}
+
+_M.name = "auto_reload"
+_M.version = "0.1.0"
+_M.description = "lua文件变动自动reload, 使实时生效"
+
+local hammerspoon_path = os.getenv("HOME") .. "/.hammerspoon/"
+
+local function reload(files)
+    local doReload = false
+    for _, file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+
+    if doReload then
+        hs.reload()
+    end
+end
+
+hs.pathwatcher.new(hammerspoon_path, reload):start()
+
+hs.alert.show("Hammerspoon config reloaded")
+
+return _M
