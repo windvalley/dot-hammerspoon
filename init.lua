c1fcba573f689a60559e63fa365da9611cdad541
windvalley <i@sre.im>
2022-12-21 22:14:41 +0800

    refactor: optimize window management

--- init.lua
+++ init.lua
@@ -8,9 +8,6 @@ _M.author = "XG <levinwang6@gmail.com>"
 _M.license = "MIT"
 _M.homepage = "https://github.com/windvalley/dot-hammerspoon"
 
--- 每次按快捷键时显示快捷键alert消息持续的秒数, 0 为禁用.
-hs.hotkey.alertDuration = 0
-
 -- app切换
 require("apps_switch")
 
