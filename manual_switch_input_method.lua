86457e7fdc4f1ca0dee560a4774350e9541f6184
windvalley <levinwang6@gmail.com>
2022-12-27 17:10:32 +0800

    feat: add auto switch input method according to the application

--- manual_switch_input_method.lua
+++ manual_switch_input_method.lua
@@ -1,26 +1,23 @@
 local _M = {}
 
 _M.name = "manual_switch_input_method"
-_M.version = "0.1.1"
 _M.description = "明确指定切换到某个输入法"
 
-local input_methods = require("keybindings_config").manual_input_methods
+local manual_input_methods = require "keybindings_config".manual_input_methods
+local input_method_lib = require "input_method_lib"
 
-local pinyin = "com.apple.inputmethod.SCIM.ITABC"
-local abc = "com.apple.keylayout.ABC"
-
--- 切换到简体拼音
-local function switch_pinyin()
-    hs.keycodes.currentSourceID(pinyin)
-end
-
--- 切换到ABC
-local function switch_abc()
-    hs.keycodes.currentSourceID(abc)
-end
-
-hs.hotkey.bind(input_methods.abc.prefix, input_methods.abc.key, input_methods.abc.message, switch_abc)
-
-hs.hotkey.bind(input_methods.pinyin.prefix, input_methods.pinyin.key, input_methods.pinyin.message, switch_pinyin)
+hs.fnutils.each(
+    manual_input_methods,
+    function(item)
+        hs.hotkey.bind(
+            item.prefix,
+            item.key,
+            item.message,
+            function()
+                input_method_lib.switch_input_method(item.input_method)
+            end
+        )
+    end
+)
 
 return _M
