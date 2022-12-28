37c3157008d5e187b28558acd5c689a668e2a7e7
windvalley <levinwang6@gmail.com>
2022-12-28 09:51:46 +0800

    feat: add hs.logger to manage logs

--- app_launch.lua
+++ app_launch.lua
@@ -1,19 +1,20 @@
 local _M = {}
 
-_M.__index = _M
-
 _M.name = "app_launch"
-_M.version = "0.1.0"
 _M.description = "app启动或切换"
 
 local apps = require "keybindings_config".apps
+
+local log = hs.logger.new("appLaunch")
 
 -- App显示或隐藏
 local function toggleAppByBundleId(bundleID)
     local frontApp = hs.application.frontmostApplication()
     if frontApp:bundleID() == bundleID and frontApp:focusedWindow() then
+        log.d(string.format("hide app: %s", bundleID))
         frontApp:hide()
     else
+        log.d(string.format("launch app: %s", bundleID))
         hs.application.launchOrFocusByBundleID(bundleID)
     end
 end
